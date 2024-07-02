08ce6da2d82b57c79fdf6e5efd13b5e2089d543c
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-07-02 15:41:04 +0200

    poprawalogin

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
                       <h4 className="mt-1 mb-5 pb-1">TaskHero</h4>
                     </div>
                     <p className="mb-5">Please login to your account</p>
-                    <Form route="/api/token/" />
+                    <Form route="/api/token/" method="login"/>
                     <div className="d-flex align-items-center justify-content-center pb-4">
                       <p className="mb-0 me-2">Don't have an account?</p>
                       <button
