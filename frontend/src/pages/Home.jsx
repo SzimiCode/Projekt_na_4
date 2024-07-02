c5cda969ea9b26e43f0bb3fcfb1c97dbfc9a12c9
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-07-02 19:03:04 +0200

    Update Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,7 +27,7 @@ function Home() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" href="../MainView/mainView.html">Home</a>
+                <a className="nav-link" href="../">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="../ToDoListView/ToDoListView.html">ToDoList</a>
