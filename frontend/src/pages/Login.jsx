02c5041482f5390f75043f6df99175e5b759c79b
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-07-02 15:33:55 +0200

    ds

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import "../styles/style.css";
+import "../styles/style.css"; // Upewnij się, że ścieżka do pliku CSS jest poprawna
 import Form from "../components/Form";
 
 function Login() {
@@ -22,7 +22,17 @@ function Login() {
                       />
                       <h4 className="mt-1 mb-5 pb-1">TaskHero</h4>
                     </div>
-                    <Form route="/api/token/" method="login" />
+                    <p className="mb-5">Please login to your account</p>
+                    <Form route="/api/token/" />
+                    <div className="d-flex align-items-center justify-content-center pb-4">
+                      <p className="mb-0 me-2">Don't have an account?</p>
+                      <button
+                        type="button"
+                        className="btn btn-outline-dark"
+                      >
+                        Create new
+                      </button>
+                    </div>
                   </div>
                 </div>
                 <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
