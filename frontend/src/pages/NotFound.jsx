bb4defdcc6c0234236d27bc703e18ef50e46a8c4
Kacper <74537153+KasmyrA@users.noreply.github.com>
2024-07-02 13:53:26 +0200

    Something is even working

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,14 @@
-function NotFound(){
-    return <div>Not found</div>
+import "../styles/style.css"
+function NotFound() {
+    return <div>
+        <div className="text-center">
+        <h1>404 Not Found</h1>
+        <p>The page you're looking for doesn't exist!</p>
+        </div>
+            
+
+        
+    </div>
 }
 
-export default NotFound;
+export default NotFound
