2710d217d9af0302c925ec5c1414036567311fc8
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-07-02 15:34:25 +0200

    Form update

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function Form({ route, method }) {
+function Form({ route }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -25,7 +25,6 @@ function Form({ route, method }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className="mb-5">Please login to your account</p>
       <div className="form-outline mb-4">
         <input
           type="email"
@@ -48,15 +47,11 @@ function Form({ route, method }) {
         <label className="form-label" htmlFor="form2Example22">Password</label>
       </div>
       <div className="text-center pt-1 mb-5 pb-1">
-        <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">
+        <button
+          className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+          type="submit"
+        >
           Log in
-        </button>
-        <a className="text-muted" href="#!">Forgot password?</a>
-      </div>
-      <div className="d-flex align-items-center justify-content-center pb-4">
-        <p className="mb-0 me-2">Don't have an account?</p>
-        <button type="button" className="btn btn-outline-dark">
-          Create new
         </button>
       </div>
     </form>
